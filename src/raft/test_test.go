e1f9f3e99262fcd719cfd7737e1c52e7a1574395
NishikiChisato <NishikiChisato@outlook.com>
2024-02-04 17:36:00 +0800

    restore file

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -503,7 +503,6 @@ func TestBackup2B(t *testing.T) {
 
 	// put leader and one follower in a partition
 	leader1 := cfg.checkOneLeader()
-
 	cfg.disconnect((leader1 + 2) % servers)
 	cfg.disconnect((leader1 + 3) % servers)
 	cfg.disconnect((leader1 + 4) % servers)
@@ -530,7 +529,6 @@ func TestBackup2B(t *testing.T) {
 
 	// now another partitioned leader and one follower
 	leader2 := cfg.checkOneLeader()
-
 	other := (leader1 + 2) % servers
 	if leader2 == other {
 		other = (leader2 + 1) % servers
